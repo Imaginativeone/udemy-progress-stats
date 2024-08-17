36d698bd0edccf42f52aff1aaa5f14685323e7b0
Doug Franklin <doug@dougfranklin.com>
2024-08-17 13:55:23 -0400

    Data to UI

--- app/Components/main-ui.tsx
+++ app/Components/main-ui.tsx
@@ -4,9 +4,20 @@ const MainUI = ({data}: any) => {
 
     console.log('data', data)
 
+    const dataArray = data.split('\n')
+
+    console.log('dataArray', dataArray)
+
     return (
         <>
             <h1>Main UI</h1>
+            {
+                dataArray.map((line:any) => {
+                    return (
+                        <div>{ line }</div>
+                    )
+                })
+            }
         </>
     )
 }
