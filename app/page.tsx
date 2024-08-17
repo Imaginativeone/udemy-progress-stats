8ec5754808c5a565c9aa33857b9a8e9fdfb73a74
Doug Franklin <doug@dougfranklin.com>
2024-08-17 13:31:43 -0400

    Data Acquired

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
 
+import data from '@/app/data/web-dev-bootcamp'
+import MainUI from "./Components/main-ui";
+
+console.log('Hello')
+
 export default function Home() {
+
+  console.log('data', data)
+
   return (
     <>
-      <h1>Home</h1>
+      <h1>Home - Hello</h1>
+      <MainUI data={data}></MainUI>
     </>
   );
 }
