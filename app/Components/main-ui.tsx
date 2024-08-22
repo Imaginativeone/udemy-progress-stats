cdd3bb996726c8a36133c027048029ddf4f6b01f
Doug Franklin <doug@dougfranklin.com>
2024-08-22 07:43:33 -0400

    Sample functionality

--- app/Components/main-ui.tsx
+++ app/Components/main-ui.tsx
@@ -1,18 +1,45 @@
 "use client";
 
-const MainUI = ({ data }: any) => {
-  console.log("data", data);
+import { Tree } from 'react-arborist'
 
-  const dataArray = data.split("\n");
+const data = [
+    { id: "1", name: "Unread" },
+    { id: "2", name: "Threads" },
+    {
+      id: "3",
+      name: "Chat Rooms",
+      children: [
+        { id: "c1", name: "General" },
+        { id: "c2", name: "Random" },
+        { id: "c3", name: "Open Source Projects" },
+      ],
+    },
+    {
+      id: "4",
+      name: "Direct Messages",
+      children: [
+        { id: "d1", name: "Alice" },
+        { id: "d2", name: "Bob" },
+        { id: "d3", name: "Charlie" },
+      ],
+    },
+  ];
 
-  console.log("dataArray", dataArray);
+const MainUI = () => {
+
+//   console.log("data", data);
+
+//   const dataArray = data.split("\n");
+
+//   console.log("dataArray", dataArray);
 
   return (
     <>
       <h1>Main UI</h1>
-      {dataArray.map((line: any, index: any) => {
+      <Tree initialData={data} />
+      {/* {dataArray.map((line: any, index: any) => {
         return <div key={index}>{line}</div>;
-      })}
+      })} */}
     </>
   );
 };
